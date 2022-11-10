f9a2265121bb6ac4fc3ec55ddc70e125ca89b7c3
Rubotnic <info@rubotnic.com>
2022-11-11 00:31:00 +0100

    My new react project

---
+++ src/ex1-class-components/HeaderClassComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HeaderClassComponent extends Component {
+    render() {
+        return (
+            <div>
+                <h4>"Header info"</h4>
+            </div>
+        );
+    }
+}
+
+export default HeaderClassComponent;
