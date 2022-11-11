9e029e44606f118dc62518500ffffcefb61a4712
Rubotnic <info@rubotnic.com>
2022-11-11 10:06:04 +0100

    My react workshop project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import ReactDOM from 'react-dom/client';
 
 
-import App from './ex1-class-components/App';
+// import App from './ex1-class-components/App';
+import MenuList from './ex7-props/MenuList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+
+
+const listOfNames = ["Stålmannen", "Träpojken"];
+root.render(<NameListProps names={listOfNames}  />);
+
+const links = [
+    {id: 1, name: "lexicon", url:"http://www.lexicon.se"},
+    {id:2 , name: 'Oracle', url: 'https://www.oracle.com'},
+    {id:3 , name: 'Google', url: 'https://www.google.com'}
+]
+
+
+root.render(<MenuList />);
